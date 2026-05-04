56c66fcda6eeb4cd9dbab18cd91db9be3c6838db
josilva129 <joao.silva129@sptech.school>
2026-05-04 12:16:46 -0300

    correçao de bugs no login js

--- web-data-viz-main/public/js/login.js
+++ web-data-viz-main/public/js/login.js
@@ -2,9 +2,8 @@ function validaEmail() {
     let email = input_email.value
     let conter = ['@', '.com']
     let validacao = 0
-    let mensagem = ''
 
-    for (let i = 0; i <= conter.length; i++) {
+    for (let i = 0; i < conter.length; i++) {
 
         if (email.includes(conter[i])) {
             validacao++
@@ -12,31 +11,35 @@ function validaEmail() {
     }
 
     if (validacao != 2) {
-        mensagem = 'Digite um endereço de e-mail válido'
+        p_mensagem.innerHTML = 'Digite um endereço de e-mail válido'
+        return false
     }
 
-    p_mensagem.innerHTML = mensagem
+    p_mensagem.innerHTML = ''
+    return true
 }
 
 function validaSenha() {
     let senha = input_senha.value
-    let mensagem = ''
     
-    if(senha == ''){
-        mensagem = 'Informe a senha'
+    if(senha == '' || senha.length < 8){
+        div_mensagem.innerHTML = 'Informe a senha'
+        return false
     }
 
-    div_mensagem.innerHTML = mensagem
+    div_mensagem.innerHTML = ''
+    return true
 }
 
 function entrar() {
-    validaEmail()
-    validaSenha()
 
-     if(validaEmail() && validaSenha()){
+    let emailValido = validaEmail()
+    let senhaValida = validaSenha()
+    
+     if(emailValido && senhaValida){
         setTimeout(() => {
             window.location.href = './home.html'
-        }, '1000')
+        }, 1000)
     }
    
 }
