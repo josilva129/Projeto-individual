3109e31df5413346658873b35a18ea024e289510
josilva129 <joao.silva129@sptech.school>
2026-05-06 15:28:22 -0300

    graficos

---
+++ web-data-viz-main/public/js/dashboard.js
@@ -0,0 +1,120 @@
+const ctxFilmes = document.getElementById('filmesPopulares');
+
+new Chart(ctxFilmes, {
+  type: 'bar',
+  data: {
+    labels: [
+    "Relíquias da Morte II",
+    "Relíquias da Morte I",
+    "Cálice de Fogo",
+    "Enigma do Príncipe",
+    "Câmara Secreta",
+    "Pedra Filosofal",
+    "Ordem da Fênix",
+    "Prisioneiro de Azkaban",
+    
+      
+    ],
+    datasets: [{
+      label: 'Popularidade (score)',
+      data: [96, 90, 88, 83, 82, 80, 78, 76],
+      backgroundColor: '#d4af37',
+      borderRadius: 3,
+    }]
+  },
+  options: {
+    indexAxis: 'y',
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Filmes mais populares de Harry Potter',
+        color: '#fff'
+      }
+    },
+    scales: {
+      x: {
+        beginAtZero: true,
+        ticks: {
+          color: '#ffffff' 
+        },
+        grid: {
+          color: 'rgba(255,255,255,0.2)'
+        }
+      },
+      y: {
+        ticks: {
+          color: '#ffffff'
+        },
+        grid: {
+          color: 'rgba(255,255,255,0.1)'
+        },
+       
+      }
+    }
+  }
+});
+
+const ctxFeiticos = document.getElementById('feiticosPopulares');
+
+new Chart(ctxFeiticos, {
+  type: 'bar',
+  data: {
+    labels: [
+      "Expelliarmus",
+      "Estupefaça",
+      "Expecto Patronum",
+      "Avada Kedavra",
+      "Crucio",
+      "Imperio",
+      "Accio",
+      "Lumos",
+    ],
+    datasets: [{
+      label: 'Popularidade (score)',
+      data: [24, 19, 11, 8, 5, 4, 4, 4],
+      backgroundColor: '#d4af37',
+      borderRadius: 3,
+    }]
+  },
+  options: {
+    indexAxis: 'y',
+    responsive: true,
+
+    plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Feitiços mais populares de Harry Potter',
+        color: '#fff'
+      }
+    },
+
+    scales: {
+      x: {
+        beginAtZero: true,
+        ticks: {
+          color: '#ffffff'
+        },
+        grid: {
+          color: 'rgba(255,255,255,0.2)'
+        }
+      },
+      y: {
+        ticks: {
+          color: '#ffffff'
+        },
+        grid: {
+          color: 'rgba(255,255,255,0.1)'
+        },
+        categoryPercentage: 0.7,
+        barPercentage: 0.8
+      }
+    }
+  }
+});
