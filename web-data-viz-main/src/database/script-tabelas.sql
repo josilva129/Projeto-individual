3a21923a40557c92618ebb9a490fe0518140b8ad
josilva129 <joao.silva129@sptech.school>
2026-05-07 11:19:47 -0300

    alteraçao no banco de dados

--- web-data-viz-main/src/database/script-tabelas.sql
+++ web-data-viz-main/src/database/script-tabelas.sql
@@ -20,41 +20,13 @@ CREATE TABLE usuario (
     casa VARCHAR(45)
 );
 
-CREATE TABLE quizzes (
-    idQuizzes INT PRIMARY KEY AUTO_INCREMENT,
-    titulo VARCHAR(45),
-    tema VARCHAR(45)
-);
-
-CREATE TABLE perguntas (
-    idPerguntas INT PRIMARY KEY AUTO_INCREMENT,
-    enunciado VARCHAR(60),
-    fk_quiz INT,
-    CONSTRAINT fk_pergunta_quiz
-        FOREIGN KEY (fk_quiz)
-        REFERENCES Quizzes(idQuizzes)
-);
-
-CREATE TABLE respostas (
-    idRespostas INT PRIMARY KEY AUTO_INCREMENT,
-    texto VARCHAR(45),
-    correta TINYINT,
-    fk_perguntas INT,
-    CONSTRAINT fk_resposta_pergunta
-        FOREIGN KEY (fk_perguntas)
-        REFERENCES Perguntas(idPerguntas)
-);
 
 CREATE TABLE resultado (
     idResultado INT PRIMARY KEY AUTO_INCREMENT,
     pontuacao INT,
+    dtQuiz DATETIME DEFAULT CURRENT_TIMESTAMP,
     fk_usuario INT,
-    fk_resul_quiz INT,
     CONSTRAINT fk_resultado_usuario
         FOREIGN KEY (fk_usuario)
-        REFERENCES Usuario(idUsuario),
-    CONSTRAINT fk_resultado_quiz
-        FOREIGN KEY (fk_resul_quiz)
-        REFERENCES Quizzes(idQuizzes)
+        REFERENCES usuario(idUsuario)
 );
-
